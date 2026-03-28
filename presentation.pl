ffc3acc1fdf48ae8337fd6a698a55da037dac11e
Marinela <Marinelasuelly456gmail.com>
2026-03-28 01:09:23 +0000

    Mudei o nome para presentation.pl

---
+++ presentation.pl
@@ -0,0 +1,17 @@
+$name = "Marinela";
+$age = 25;
+$SO_grade = 12.45;
+
+print "My name is: $name\n";
+print "I'm $age years old.\n";
+print "I got $SO_grade marks in the subject of operating systems.\n\n";
+
+
+################# Arrays in Perl ################
+
+@numbers = (20,40,10); 
+@words ="one","two","three";
+
+###################### Hashees in Perl ##########
+
+%countries = ('India','IND','United States of America', 'USA', 'Canda', 'CAN');
